fffe9dcd7dd513da2d5265e7814cb14bedea7c33
Abozaid <abuzeid7@gmail.com>
2019-01-02 23:41:46 +0200

    Initial Commit

---
+++ ProblemSolving_HackerRank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProblemSolving_HackerRank
+//
+//  Created by abuzeid on 1/2/19.
+//  Copyright © 2019 abuzeid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
