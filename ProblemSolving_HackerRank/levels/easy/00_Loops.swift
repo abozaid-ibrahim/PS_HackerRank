f04c21e91bc2ceed9d7dc8dbe74d09cadea8a64d
abuzeid <abuzeid@Abuzeid-MacBook-Pro.local>
2019-01-17 12:45:19 +0200

    problem 3 looping 5

---
+++ ProblemSolving_HackerRank/levels/easy/00_Loops.swift
@@ -0,0 +1,23 @@
+//
+//  00_Loops.swift
+//  ProblemSolving_HackerRank
+//
+//  Created by abuzeid on 1/17/19.
+//  Copyright © 2019 abuzeid. All rights reserved.
+//
+
+import Foundation
+
+class Multiplier{
+    
+    func getMultipliers(_ item:Int){
+        guard item >= 2 && item < 20 else{
+            return
+        }
+        for indx in 1...10{
+            print("\(item) * \(indx) = \(item*indx)")
+        }
+    }
+    
+}
+
